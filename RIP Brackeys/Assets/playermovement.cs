b61348f650df94abc0960105657667bc46429cac
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2021-04-21 11:53:29 -0700

    .

---
+++ RIP Brackeys/Assets/playermovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float 2000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.AddForce(0, 200, 500);
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
