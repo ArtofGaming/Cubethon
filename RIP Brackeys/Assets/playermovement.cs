4ad9ac110e3dfbbeb6ff5f46da1020da2be338ec
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2021-04-23 11:53:38 -0700

    finished ep 4

--- RIP Brackeys/Assets/playermovement.cs
+++ RIP Brackeys/Assets/playermovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float 2000f;
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,15 @@ public class playermovement : MonoBehaviour
     
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
