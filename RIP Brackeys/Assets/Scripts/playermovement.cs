6595eec4b5563bbcdfeb8149f9855122f6e25eb9
Campbell Turner <turner4965@gmail.com>
2023-01-22 21:49:22 -0700

    seek n flee

--- RIP Brackeys/Assets/Scripts/playermovement.cs
+++ RIP Brackeys/Assets/Scripts/playermovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forwardForce = 2000f;
+    public float forwardForce = 200f;
     public float sidewayForce = 500f;
 
     // Start is called before the first frame update
